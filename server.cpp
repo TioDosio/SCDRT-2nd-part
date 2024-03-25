01800507ebb40a3350f90a76fd59f590869d626d
andre <andre.teodosio@tecnico.ulisboa.pt>
2024-03-25 09:46:07 +0000

    commented code

--- server.cpp
+++ server.cpp
@@ -1,32 +1,33 @@
 #include <iostream>
 #include <memory>
 #include <boost/asio.hpp>
+#include "Serial.cpp" // Assuming this file contains additional functionality related to serial communication.
 
-using namespace boost::asio;
+using namespace boost::asio; // Import Boost.Asio symbols into the global namespace.
 
-constexpr size_t MAX_SZ{128};
+constexpr size_t MAX_SZ{128}; // Maximum size for read/write buffers.
 
 class session : public std::enable_shared_from_this<session>
 {
-    ip::tcp::socket sock;
-    char rbuf[MAX_SZ] = {0}, tbuf[MAX_SZ] = {0};
+    ip::tcp::socket sock;                        // TCP socket for this session.
+    char rbuf[MAX_SZ] = {0}, tbuf[MAX_SZ] = {0}; // Read and write buffers.
 
 public:
-    session(ip::tcp::socket s) : sock(std::move(s)) {}
+    session(ip::tcp::socket s) : sock(std::move(s)) {} // Constructor taking a socket and moving it into the session.
 
-    void start()
+    void start() // Method to start reading from the socket.
     {
-        auto self{shared_from_this()};
-        sock.async_read_some(buffer(rbuf, MAX_SZ),
-                             [this, self](boost::system::error_code ec, size_t sz)
+        auto self{shared_from_this()};                                             // Capture a shared pointer to this session.
+        sock.async_read_some(buffer(rbuf, MAX_SZ),                                 // Asynchronously read data into rbuf.
+                             [this, self](boost::system::error_code ec, size_t sz) // Callback when data is read.
                              {
-                                 if (!ec)
+                                 if (!ec) // If no error.
                                  {
-                                     rbuf[sz] = 0;
-                                     std::cout << "ack" << std::endl;
-                                     async_write(sock, buffer(rbuf, sz),
-                                                 [this, self](boost::system::error_code ec, std::size_t /* sz */) {});
-                                     start();
+                                     rbuf[sz] = 0;                                                                     // Null-terminate received data.
+                                     std::cout << "ack" << std::endl;                                                  // Print acknowledgment.
+                                     async_write(sock, buffer(rbuf, sz),                                               // Asynchronously write back the received data.
+                                                 [this, self](boost::system::error_code ec, std::size_t /* sz */) {}); // Empty callback.
+                                     start();                                                                          // Restart reading.
                                  }
                              });
     }
@@ -34,33 +35,33 @@ public:
 
 class server
 {
-    ip::tcp::acceptor acc;
+    ip::tcp::acceptor acc; // Acceptor for incoming connections.
 
-    void start_accept()
+    void start_accept() // Method to start accepting connections.
     {
         acc.async_accept(
-            [this](boost::system::error_code ec, ip::tcp::socket sock)
+            [this](boost::system::error_code ec, ip::tcp::socket sock) // Asynchronously accept connections.
             {
-                if (!ec)
+                if (!ec) // If no error.
                 {
-                    std::make_shared<session>(std::move(sock))->start();
+                    std::make_shared<session>(std::move(sock))->start(); // Create a session for the accepted connection and start it.
                 }
-                start_accept();
+                start_accept(); // Restart accepting connections.
             });
     }
 
 public:
     server(io_context &io, unsigned short port)
-        : acc{io, ip::tcp::endpoint{ip::tcp::v4(), port}}
+        : acc{io, ip::tcp::endpoint{ip::tcp::v4(), port}} // Constructor initializing the acceptor with IPv4 and given port.
     {
-        start_accept();
+        start_accept(); // Start accepting connections.
     }
 };
 
 int main()
 {
-    io_context io;
-    server s(io, 10000);
-    io.run();
+    io_context io;       // Create an IO context.
+    server s(io, 10000); // Create a server listening on port 10000.
+    io.run();            // Run the IO context's event loop.
     return 0;
 }
